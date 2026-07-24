ae9912de4c993d52e4c8572437de44ba2449fbfa
Alonepard10501 <falkcarstensen@web.de>
2026-07-25 00:02:43 +0200

    Web v2.2.0: Team-Runden (mehrere Gruppen, Teampunkte, Team-Statistik), Englisch ueberall, Einstellungs-Navigation-Fix, Aufraeumen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "267039891" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1223325168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
