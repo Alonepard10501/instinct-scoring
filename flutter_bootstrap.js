4a5dd0ff1bea9090ecd4ac2e1ade5f5d5e8179d9
Alonepard10501 <falkcarstensen@web.de>
2026-07-28 09:42:09 +0200

    Release 1.3.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "945958627" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4029197502" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
